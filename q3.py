27fc62dd482501b0540c37ce6c0ea49960444ba6
annebozack <anne.bozack@gmail.com>
2015-11-15 15:45:02 -0500

    question 3

--- q3.py
+++ q3.py
@@ -9,11 +9,11 @@ path=sys.argv[1]
 
 # Failed reads
 
-os.system('poretools yield_plot --plot-type basepairs --saveas plot3_fail.png '+path+'downloads/fail/2D/')
+os.system('poretools yield_plot --plot-type basepairs --theme-bw --saveas plot3_fail.png '+path+'downloads/fail/2D/')
 
 # Passed reads
 
-os.system('poretools yield_plot --plot-type basepairs --saveas plot3_pass.png '+path+'downloads/pass/2D/')
+os.system('poretools yield_plot --plot-type basepairs --theme-bw --saveas plot3_pass.png '+path+'downloads/pass/2D/')
 
 
 
