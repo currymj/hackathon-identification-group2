486d0be8076146c3ec658f16c9bec91aaf68d68a
annebozack <anne.bozack@gmail.com>
2015-11-19 17:21:30 -0500

    Q2.py

---
+++ q2.py
@@ -0,0 +1,50 @@
+###########################################################################
+# Number 2
+###########################################################################
+
+import sys
+import re
+import os
+
+path = sys.argv[1]
+
+channel_dict = {}
+
+def get_channels(s):
+	file_list = os.listdir(s)
+	for f in file_list:
+		channel = re.search('ch(\d+)_file', f)
+		if channel:
+			if channel.group(1) in channel_dict.keys():
+				channel_dict[channel.group(1)] += 1
+			else:		
+				channel_dict[channel.group(1)] = 1
+	return channel_dict
+
+# For pass
+channel_list = get_channels(''+path+'downloads/pass/')
+
+# For fail
+channel_list = get_channels(''+path+'downloads/fail/')
+
+# Average
+total = 0
+for i in channel_list:
+	total = total + channel_list[i]
+avr = float(total)/len(channel_list)
+
+file = open(‘Q2channeloutput.txt', 'w')
+
+file.write('Average reads per channel: ' + str(avr) + '\nPore with most reads: ' + str(max(channel_list, key=channel_list.get)) + '\nNumber of reads: ' + str(channel_list[max(channel_list, key=channel_list.get)]))
+
+file.close()
+
+
+
+	
+		
+
+
+
+
+
