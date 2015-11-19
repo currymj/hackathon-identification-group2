09fd46f18247747ca400a5a7e06737685db92907
annebozack <anne.bozack@gmail.com>
2015-11-19 17:18:08 -0500

    Update q5.py

--- q5.py
+++ q5.py
@@ -9,27 +9,21 @@ path=sys.argv[1]
 
 file = open('readlengths.txt', 'w')
 
-# Passed reads
-# 2D
+# Passed reads - 2D
 
 os.system('poretools winner --type 2D '+path+'downloads/pass/ >'+path+'downloads/pass/long2DPass.txt')
 
 f = open(''+path+'downloads/pass/long2DPass.txt')
 lines = f.readlines()
-file.write('Longest passed 2D: ' + lines[0])
-file.write('Length: ' + str(len(lines[1])))
-file.write('')
+file.write('Longest passed 2D: ' + lines[0] + '\nLength: ' + str(len(lines[1])))
 
-# Failed reads
-# 2D
+# Failed reads - 2D
 
 os.system('poretools winner --type 2D '+path+'downloads/fail/ >'+path+'downloads/fail/long2DFail.txt')
 
 f = open(''+path+'downloads/fail/long2DFail.txt')
 lines = f.readlines()
-file.write('Longest failed 2D: ' + lines[0])
-file.write('Length: ' + str(len(lines[1])))
-file.write('')
+file.write('\nLongest failed 2D: ' + lines[0] + '\nLength: ' + str(len(lines[1])))
 
 file.close()
 
