c590f34765e5c9a908192b47e253cf864961a26c
annebozack <anne.bozack@gmail.com>
2015-11-20 07:43:24 -0500

    Update group2_report1_question4.py

--- group2_report1_question4.py
+++ group2_report1_question4.py
@@ -10,10 +10,10 @@ path=sys.argv[1]
 
 # Failed 2D reads
 
-os.system('poretools hist --saveas plot4_fail_2D.png '+path+'downloads/fail/')
+os.system('poretools hist --saveas plot4_fail_2D.png '+path+'fail/')
 
 # Passed 2D reads
 
-os.system('poretools hist --saveas plot4_pass_2D.png '+path+'downloads/pass/')
+os.system('poretools hist --saveas plot4_pass_2D.png '+path+'pass/')
 
 
