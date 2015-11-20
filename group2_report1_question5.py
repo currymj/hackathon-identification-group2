2bafcf62c24a3e6955faf14f35544cf02d507045
annebozack <anne.bozack@gmail.com>
2015-11-20 08:57:10 -0500

    Update group2_report1_question5.py

--- group2_report1_question5.py
+++ group2_report1_question5.py
@@ -11,17 +11,17 @@ file = open('Q5readlengths.txt', 'w')
 
 # Passed reads - 2D
 
-os.system('poretools winner --type 2D '+path+'pass/ >'+path+'pass/long2DPass.txt')
+os.system('poretools winner --type 2D '+path+'downloads/pass/ >'+path+'downloads/pass/long2DPass.txt')
 
-f = open(''+path+'pass/long2DPass.txt')
+f = open(''+path+'downloads/pass/long2DPass.txt')
 lines = f.readlines()
 file.write('Longest passed 2D: ' + lines[0] + '\nLength: ' + str(len(lines[1])))
 
 # Failed reads - 2D
 
-os.system('poretools winner --type 2D '+path+'fail/ >'+path+'fail/long2DFail.txt')
+os.system('poretools winner --type 2D '+path+'downloads/fail/ >'+path+'downloads/fail/long2DFail.txt')
 
-f = open(''+path+'fail/long2DFail.txt')
+f = open(''+path+'downloads/fail/long2DFail.txt')
 lines = f.readlines()
 file.write('\n\nLongest failed 2D: ' + lines[0] + '\nLength: ' + str(len(lines[1])))
 
